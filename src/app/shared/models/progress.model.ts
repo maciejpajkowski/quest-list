f476b93dc6ccdbc21f00d2da080e64c9be83791a
maciejpajkowski <32037952+maciejpajkowski@users.noreply.github.com>
2021-03-08 15:55:07 +0100

    Added a progress model

---
+++ src/app/shared/models/progress.model.ts
@@ -0,0 +1,8 @@
+export class Progress {
+    constructor(
+        public tasksCreated: number,
+        public tasksCompleted: number,
+        public totalExperience: number,
+        public totalGold: number
+    ) {}
+}
